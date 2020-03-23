70a4b60cd39097cb68bd3d6e070bc5bbc834d239
royyandzakiy <royyandzakiy@students.itb.ac.id>
2020-03-24 00:42:53 +0700

    fixed problem of publishing empty other node datagramTable

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,20 @@ const int ss = 5;
 const int rst = 14;
 const int dio0 = 2; //*/
 
+// === WiFi
+// #include <ESP8266WiFi.h> // use for ESP8266
+#include <WiFi.h> // use for anything else
+#include <PubSubClient.h>
+
+const char* ssid = "Ciheulang 3";
+const char* password = "CBnotiga";
+const char* mqtt_server = "192.168.1.7";
+String topicPub = "theSentinel/nodeHub";
+String topicSub = "theSentinel/nodeHubDebug";
+
+WiFiClient thisWifiClient;
+PubSubClient pubSubClient(thisWifiClient);
+
 // === OTHERS
 unsigned long lastSecond;
 
@@ -30,6 +44,82 @@ DatagramTable thisDatagramTable;
 
 
 // ----------------------
+
+// === WiFi
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void publish_packet(String _datagramTableString) {
+  String tempMsgString = "hello from nodeId " + (String) nodeId + "::" + _datagramTableString;
+  char msg[tempMsgString.length() + 1];
+  strcpy(msg, tempMsgString.c_str());
+  pubSubClient.publish(topicPub.c_str(), msg);
+  Serial.println("published::" + (String) msg);
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!pubSubClient.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a pubSubClient ID
+    String clientId = "NodeClient-";
+    clientId += (String) nodeId;
+    // Attempt to connect
+    if (pubSubClient.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish
+      publish_packet(thisDatagramTable.get_to_string());
+      // ... and resubscribe
+      pubSubClient.subscribe(topicSub.c_str());
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(pubSubClient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 // === LoRa
 void lora_setup() {
@@ -93,6 +183,9 @@ void retrieve_packet() {
     Serial.print("updated: ");
     thisDatagramTable.print();
     Serial.println();
+
+    // publish thisDatagramTable to mqtt
+    publish_packet(datagramTableStringPacket);
   }
 }
 
@@ -120,9 +213,20 @@ void setup() {
 
   Serial.println("=== LoRa Node ===");
   lastSecond = millis();
+
+  // === WiFi Setup
+  setup_wifi();
+  pubSubClient.setServer(mqtt_server, 1883);
+  pubSubClient.setCallback(callback);
 }
 
 void loop() { 
+    // pastikan wifi terhubung
+    if (!pubSubClient.connected()) {
+      reconnect();
+    }
+    pubSubClient.loop();
+
     // listen paket lora tiap saat
     listen_packet();
 
@@ -138,7 +242,10 @@ void loop() {
       // jumlah paket yang dikirim dan publish dalam satu waktu, ubah jika dirasa ada paket loss
       int iterasiPengiriman = 1;
       for (int i=0; i<iterasiPengiriman; i++) {
-        send_packet();
+        send_packet();       
+
+        String datagramTableString = thisDatagramTable.get_to_string();
+        publish_packet(datagramTableString);
         delay(50);
       }
     }
